8ef001d60f11d717d4252817eec8305ab430fe2b
joemacdog <1730114644@qq.com>
2015-11-07 21:02:11 +0800

    text

---
+++ text/text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by mac-dog on 15/11/7.
+//  Copyright © 2015年 mac-dog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ text/text/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  text
+//
+//  Created by mac-dog on 15/11/7.
+//  Copyright © 2015年 mac-dog. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
